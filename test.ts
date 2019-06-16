9c4018148f06288a85c653f94a4af3017d0e674f
Magnus Thor <magnus.thor74@gmail.com>
2019-06-16 13:40:09 +0200

    Example updated, now with procedual textures

--- test.ts
+++ test.ts
@@ -1,4 +1,9 @@
 import { Compressor } from './index';
-
 // packs foo.js into output.png.html
-Compressor.Pngify("foo.js","output.png.html",`<style>body {margin:0;padding:0}</style><canvas style="width:100%;height:100vh;left:0;position:absolute" id="webgl" width="1280" height="720"/><`);
+
+let html = `<style>body {margin:0;padding:0}</style><canvas style="width:100%;height:100vh;left:0;position:absolute" id="webgl" width="1280" height="720"/><`;
+
+
+Compressor.Pngify("foo.js",
+"output.png.html",
+html)
